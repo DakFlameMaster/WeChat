b98415d0c2d7d5805becd978ae40ee476d08961c
NTR_biss <t1146973579@163.com>
2024-08-01 15:39:00 +0800

    init submit

--- js/player/index.js
+++ js/player/index.js
@@ -17,7 +17,7 @@ export default class Player extends Sprite {
     super(PLAYER_IMG_SRC, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     // 玩家默认处于屏幕底部居中位置
-    this.x = 0
+    this.x = screenWidth / 2 - this.width / 2
     this.y = screenHeight - this.height - 30
 
     // 用于在手指移动的时候标识手指是否已经在飞机上了
